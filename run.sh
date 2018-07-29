e4694f3a09fa23a6795703f7a8df8ebf8cc4a8f3
vityok <vityok@localhost>
2018-07-30 01:22:00 +0300

    improvements to the startup script, now able to deal with external Derby installation and alert when derby.jar not found

--- run.sh
+++ run.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-DERBY_HOME="$JAVA_HOME/db"
-DERBY_JAR="$DERBY_HOME/lib/derby.jar"
+if [ -z "${DERBY_HOME}" ]; then
+    DERBY_HOME="${JAVA_HOME}/db"
+fi
 
-echo "Derby is located at: $DERBY_JAR"
+DERBY_JAR="${DERBY_HOME}/lib/derby.jar"
 
-OPML_FILE="$HOME/.novinar/feeds.opml"
-FEEDS_DB="$HOME/.novinar/feeds_db"
+echo "Using derby.jar location: ${DERBY_JAR}"
 
-java -cp $DERBY_JAR:build/jar/novinar.jar \
-     "-Dorg.bb.vityok.novinar.opml_file=$OPML_FILE" \
-     "-Dorg.bb.vityok.novinar.db_dir=$FEEDS_DB" \
+if [ ! -f "${DERBY_JAR}" ]; then
+    echo "Could not find Apache DB/Derby JAR file at the specified location. Aborting startup"
+    exit 1
+fi
+
+OPML_FILE="${HOME}/.novinar/feeds.opml"
+FEEDS_DB="${HOME}/.novinar/feeds_db"
+
+java -cp "${DERBY_JAR}:build/jar/novinar.jar" \
+     "-Dorg.bb.vityok.novinar.opml_file=${OPML_FILE}" \
+     "-Dorg.bb.vityok.novinar.db_dir=${FEEDS_DB}" \
      org.bb.vityok.novinar.Main
-
-     
